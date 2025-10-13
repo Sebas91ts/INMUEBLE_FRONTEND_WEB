33cfd11a9d9eb8b60cd668f6599b1f705ae28def
Oliver <venturavalverdeo@gmail.com>
2025-10-12 23:53:33 -0400

    wip: MisInmuebles

--- src/api/desempeno/desempeno.js
+++ src/api/desempeno/desempeno.js
@@ -4,3 +4,7 @@ import axios from "../axios";
 // GET /api/desempeno/anuncios/agente/{id}/
 export const getDesempenoAgente = (agenteId) =>
   axios.get(`api/desempeno/anuncios/agente/${agenteId}/`);
+
+// POST /api/desempeno/reporte_ia_gemini/
+export const postReporteIAGemini = (payload) =>
+  axios.post(`api/desempeno/reporte_ia_gemini/`, payload);
